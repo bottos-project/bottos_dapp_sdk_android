2e72459de51ea45377db2970978a547f5ec87b6a
xionglihui <xionglihui@vip.qq.com>
2019-09-04 17:04:34 +0800

    fix bug

--- src/main/java/com/bottos/botc/sdk/net/api/ApiService.java
+++ src/main/java/com/bottos/botc/sdk/net/api/ApiService.java
@@ -1,8 +1,8 @@
 package com.bottos.botc.sdk.net.api;
 
 import com.bottos.botc.sdk.entity.BlockHeight;
-import com.bottos.botc.sdk.entity.TradeInfo;
 import com.bottos.botc.sdk.net.response.CommonResponse;
+import com.bottos.botc.sdk.net.response.SendTransactionResponse;
 import com.bottos.botc.sdk.net.response.TransactionStatusResponse;
 
 import okhttp3.RequestBody;
@@ -20,7 +20,7 @@ public interface ApiService {
     Observable<CommonResponse<BlockHeight>> requestBlockHeight();
 
     @POST("/v1/transaction/send")
-    Observable<CommonResponse<TradeInfo>> transactionSend(@Body RequestBody requestBody);
+    Observable<CommonResponse<SendTransactionResponse>> transactionSend(@Body RequestBody requestBody);
 
     @POST("/v1/transaction/status")
     Observable<CommonResponse<TransactionStatusResponse>> transactionStatus(@Body RequestBody requestBody);
